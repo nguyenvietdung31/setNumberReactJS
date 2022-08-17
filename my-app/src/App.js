1b0c0a4549e8f085e2c4d4f8bd6e9954420c2d9c
Dung Nguyen <nguyen.vietdung.3108@gmail.com>
2022-08-17 11:51:46 +0700

    final

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,36 @@
-
 import './App.css';
 import { useEffect, useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);//initialize state with initial value of 0
+  const MAX_VALUE = 10;
+  const MIN_VALUE = -10;
 
-  const [number, setNumber] = useState(0);///initialize state with initial value of 0
-  useEffect( () => { //show alert when number greater than 10 or less than -10
-    if(number>=10)
-     alert('Max value')
-    else if (number<=-10){
+  useEffect(() => { //show alert when number greater than 10 or less than -10
+    if (number >= MAX_VALUE) {
+      alert('Max value')
+    } else if (number <= MIN_VALUE) {
       alert('Min value')
-    } 
-    else {     
     }
   })
+
   const numberIncrease = () => { //increase number
-    setNumber(number+2);
+    setNumber(number + 2)
   }
 
   const numberDecrease = () => { //decrease number
-    setNumber(number-2);
+    setNumber(number - 2)
   }
 
-  return(
+  return (
     <div className="App">
-    <h2>Số: {number}</h2>
-    <br></br>
-    <button id='numberIncrease' disabled={number>=10} onClick={numberIncrease}>Cộng<br></br>+</button>
-    &emsp;
-    &emsp;
-    <button id='numberDecrease' disabled={number<=-10} onClick={numberDecrease}>Trừ<br></br>-</button>
-
+      <h2>Số: {number}</h2>
+      <br></br>
+      <button id='numberIncrease' disabled={number >= MAX_VALUE} onClick={numberIncrease}>Cộng<br></br>+</button>
+      &emsp; {/* tab character */}
+      &emsp; {/* tab character */}
+      <button id='numberDecrease' disabled={number <= MIN_VALUE} onClick={numberDecrease}>Trừ<br></br>-</button>
     </div>
-
   );
-
-
 }
 export default App;
-
-/*
-Note: khi show alert giá trị max , gắn flag max 
-gắn class, có state 
-disabled = true;
-dùng useEffect để show alert
-*/
